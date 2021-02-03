9fb192890a17d9908552e364e508d3a8f12a7de3
Denis Averin <denis.averin@aspose.com>
2021-02-03 17:07:29 +0700

    Version 21.2.0

--- src/Aspose/BarCode/Configuration.php
+++ src/Aspose/BarCode/Configuration.php
@@ -46,7 +46,7 @@ class Configuration implements JsonSerializable
      *
      * @var string
      */
-    protected $clientVersion = '20.12.0';
+    protected $clientVersion = '21.2.0';
 
     /*
      * ClientId for API
@@ -362,7 +362,7 @@ class Configuration implements JsonSerializable
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
         $report .= '    OpenAPI Spec Version: 3.0' . PHP_EOL;
-        $report .= '    SDK Package Version: 20.12.0' . PHP_EOL;
+        $report .= '    SDK Package Version: 21.2.0' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;
