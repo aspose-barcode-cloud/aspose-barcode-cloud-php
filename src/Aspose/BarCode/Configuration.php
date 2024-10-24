df857622d5fd45c3fcaa4bf25cab90b1f8847aee
Ivan Kamkin <234-Ivan.Kamkin@users.noreply.git.saltov.dynabic.com>
2024-10-24 17:22:18 +0500

    Version of SDK updated

--- src/Aspose/BarCode/Configuration.php
+++ src/Aspose/BarCode/Configuration.php
@@ -20,7 +20,7 @@ class Configuration implements JsonSerializable
      *
      * @var string
      */
-    protected $clientVersion = '24.9.0';
+    protected $clientVersion = '24.10.0';
 
     /**
      * ClientId for API
