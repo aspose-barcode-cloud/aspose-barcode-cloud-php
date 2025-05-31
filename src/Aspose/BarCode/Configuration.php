2cb17436ef25c41d05891b42a78266531c7dfc67
Denis Averin <59285247+Denis-Averin@users.noreply.github.com>
2025-05-31 16:11:29 +0500

    Update version to 24.13.0 (#84)
    * Update version to 24.13.0
    
    * Change action branches from `main` to `v3`

--- src/Aspose/BarCode/Configuration.php
+++ src/Aspose/BarCode/Configuration.php
@@ -20,7 +20,7 @@ class Configuration implements JsonSerializable
      *
      * @var string
      */
-    protected $clientVersion = '24.12.0';
+    protected $clientVersion = '24.13.0';
 
     /**
      * ClientId for API
